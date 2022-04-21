d9587ce076b7d1d574cc69508201b56954e35b39
Adam Baker <adam_baker@live.co.uk>
2022-04-21 21:47:22 +0100

    first commit

---
+++ components/MastHead.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MastHead = () => {
+  return <div>MastHead</div>;
+};
+
+export default MastHead;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <ChakraProvider>
+    return <Component {...pageProps} />
+    <NavBar />
+  </ChakraProvider>;
 }
 
-export default MyApp
+export default MyApp;
