8620c0aca6ef22400eacc3f3a1af40c981a7dabe
Adam Baker <adam_baker@live.co.uk>
2022-05-04 00:28:57 +0100

    resolve @chakra-ui/color-mode to a version smaller than in my package.json

--- pages/theme.js
+++ pages/theme.js
@@ -1,15 +1,10 @@
-// theme.js
-
-// 1. import `extendTheme` function
 import { extendTheme } from "@chakra-ui/react";
 
-// 2. Add your color mode config
 const config = {
   initialColorMode: "light",
-  useSystemColorMode: false,
+  useSystemColorMode: true,
 };
 
-// 3. extend the theme
 const theme = extendTheme({ config });
 
 export default theme;
