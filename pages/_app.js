4bceeb8f372bf93da7115313242c9216e107b272
Adam Baker <adam_baker@live.co.uk>
2022-05-04 21:00:49 +0100

    colormode fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/provider";
-import { ColorModeProvider } from "@chakra-ui/react";
+// import { ColorModeProvider } from "@chakra-ui/react";
 import { Amplify } from "aws-amplify";
 import { AmplifyProvider, Authenticator } from "@aws-amplify/ui-react";
 import "../styles/globals.css";
@@ -11,18 +11,16 @@ Amplify.configure({ ...awsExports, ssr: true });
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <ColorModeProvider
-        options={{
-          useSystsemColorMode: true,
-        }}
-      >
-        <AmplifyProvider>
-          <Authenticator.Provider>
-            <Component {...pageProps} />
-          </Authenticator.Provider>
-        </AmplifyProvider>
-      </ColorModeProvider>
+    <ChakraProvider
+      options={{
+        useSystsemColorMode: false,
+      }}
+    >
+      <AmplifyProvider>
+        <Authenticator.Provider>
+          <Component {...pageProps} />
+        </Authenticator.Provider>
+      </AmplifyProvider>
     </ChakraProvider>
   );
 }
