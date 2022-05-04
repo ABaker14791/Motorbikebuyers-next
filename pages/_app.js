27a05a08c88dd5c416f113a41a2603ece3502a47
Adam Baker <adam_baker@live.co.uk>
2022-05-04 20:55:04 +0100

    colormode fixes

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
     <ChakraProvider>
       <ColorModeProvider
         options={{
-          useSystsemColorMode: false,
+          useSystsemColorMode: true,
         }}
       >
         <AmplifyProvider>
