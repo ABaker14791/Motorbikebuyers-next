430f406adab491f6e1a40d531982fe4613e98768
Adam Baker <adam_baker@live.co.uk>
2023-01-11 22:31:52 +0000

    creating signup/login for,

--- components/auth/Login.jsx
+++ components/auth/Login.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import Styles from "../../styles/Login.module.css";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../../utils/firebase";
 
 const login = () => {
 	const auth = getAuth(app);
+	const [hasAccount, setHasAccount] = useState(true);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
@@ -21,25 +26,69 @@ const login = () => {
 				// ..
 			});
 	};
+
+	const signIn = () => {
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				// Signed in
+				const user = userCredential.user;
+				console.log(user);
+				// ...
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				alert(errorCode);
+			});
+	};
+
 	return (
 		<div className={Styles.container}>
-			<form action="" className={Styles.login__form}>
-				<label htmlFor="email">Email</label>
-				<input
-					type="text"
-					name="email"
-					placeholder="Email"
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<label htmlFor="password">Password</label>
-				<input
-					type="text"
-					name="password"
-					placeholder="Password"
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<button onClick={signUp}>Log in</button>
-			</form>
+			<div className={Styles.loginTabs}>
+				<button className={Styles.tab}>Sign In</button>
+				<button className={Styles.tab}>Create Account</button>
+			</div>
+			<div action="" className={Styles.login__form}>
+				{hasAccount ? (
+					<>
+						<label htmlFor="email">Email</label>
+						<input
+							type="text"
+							name="email"
+							placeholder="Email"
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<label htmlFor="password">Password</label>
+						<input
+							type="text"
+							name="password"
+							placeholder="Password"
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+
+						<button onClick={signIn}>Sign in</button>
+						<button onClick={() => setHasAccount(false)}>Create Account</button>
+					</>
+				) : (
+					<>
+						<label htmlFor="email">Email</label>
+						<input
+							type="text"
+							name="email"
+							placeholder="Email"
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<label htmlFor="password">Password</label>
+						<input
+							type="text"
+							name="password"
+							placeholder="Password"
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<button onClick={signUp}>Create Account</button>
+					</>
+				)}
+			</div>
 		</div>
 	);
 };
