481f86d972dfdb98559590be1755fab2014bbbf8
Adam Baker <adam_baker@live.co.uk>
2023-03-02 18:56:35 +0000

    ally change: html heading order change h4 > h3

--- components/Brands.js
+++ components/Brands.js
@@ -8,32 +8,32 @@ import Ktm from "../assets/icons/ktm.svg";
 import Triumph from "../assets/icons/triumph.svg";
 
 const Brands = () => {
-  return (
-    <div className={Styles.Container}>
-      <h2 className={Styles.Heading}>Bike Brands We Have Collected Recently</h2>
-      <h4 className={Styles.SubHeading}>
-        Below you will find some of the most popular brands that we have
-        collected through our online valuation system.
-      </h4>
-      <div className={Styles.iconContainer}>
-        <div className={Styles.iconItem}>
-          <Image src={Honda} alt="honda logo" />
-        </div>
-        <div className={Styles.iconItem}>
-          <Image src={Ducati} alt="ducati logo" />
-        </div>
-        <div className={Styles.iconItem}>
-          <Image src={Triumph} alt="triumph logo" />
-        </div>
-        <div className={Styles.iconItem}>
-          <Image src={Husqvarna} alt="husqvarna logo" />
-        </div>
-        <div className={Styles.iconItem}>
-          <Image src={Ktm} alt="ktm logo" />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={Styles.Container}>
+			<h2 className={Styles.Heading}>Bike Brands We Have Collected Recently</h2>
+			<h3 className={Styles.SubHeading}>
+				Below you will find some of the most popular brands that we have
+				collected through our online valuation system.
+			</h3>
+			<div className={Styles.iconContainer}>
+				<div className={Styles.iconItem}>
+					<Image src={Honda} alt="honda logo" />
+				</div>
+				<div className={Styles.iconItem}>
+					<Image src={Ducati} alt="ducati logo" />
+				</div>
+				<div className={Styles.iconItem}>
+					<Image src={Triumph} alt="triumph logo" />
+				</div>
+				<div className={Styles.iconItem}>
+					<Image src={Husqvarna} alt="husqvarna logo" />
+				</div>
+				<div className={Styles.iconItem}>
+					<Image src={Ktm} alt="ktm logo" />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Brands;
