d98983e192a8044af3617d6972343484dc0e8486
Adam Baker <adam_baker@live.co.uk>
2023-03-17 21:26:53 +0000

    added gtag

--- pages/article.js
+++ pages/article.js
@@ -14,16 +14,17 @@ const article = () => {
 				<p>
 					Maintaining your motorcycle is essential for safe and enjoyable
 					riding, but did you know that it can also affect the value of your
-					motorbike? Whether you're buying a motorbike or wondering how much
-					your current bike is worth, regular maintenance can make a significant
-					difference. If you're buying a motorbike, it's essential to consider
-					the bike's condition and maintenance history. A well-maintained
-					motorcycle can indicate that the previous owner cared for the bike and
-					may have fewer issues than a poorly maintained one. Additionally, if
-					you're wondering how much a motorbike is worth, the bike's condition
-					and maintenance can affect its value. One way to determine how much
-					your motorbike is worth is to use a value my motorbike tool. These
-					tools take into account the bike's make and model, age, condition, and
+					motorbike? Whether you&apos;re buying a motorbike or wondering how
+					much your current bike is worth, regular maintenance can make a
+					significant difference. If you&apos;re buying a motorbike, it&apos;s
+					essential to consider the bike&apos;s condition and maintenance
+					history. A well-maintained motorcycle can indicate that the previous
+					owner cared for the bike and may have fewer issues than a poorly
+					maintained one. Additionally, if you&apos;re wondering how much a
+					motorbike is worth, the bike&apos;s condition and maintenance can
+					affect its value. One way to determine how much your motorbike is
+					worth is to use a value my motorbike tool. These tools take into
+					account the bike&apos;s make and model, age, condition, and
 					maintenance history to provide an estimate of its value. By keeping up
 					with regular maintenance, you can potentially increase the value of
 					your motorbike and get a higher estimate when using a value my
@@ -34,11 +35,11 @@ const article = () => {
 					with worn tires, a loose chain, or leaking brake fluid may have a
 					lower value than one with properly maintained components. In
 					conclusion, maintaining your motorcycle not only ensures safe and
-					enjoyable riding but can also affect its value. Whether you're buying
-					a motorbike or wondering how much your current bike is worth, regular
-					maintenance can make a significant difference. Consider using a value
-					my motorbike tool to estimate the value of your bike and keep up with
-					regular maintenance to potentially increase its value.
+					enjoyable riding but can also affect its value. Whether you&apos;re
+					buying a motorbike or wondering how much your current bike is worth,
+					regular maintenance can make a significant difference. Consider using
+					a value my motorbike tool to estimate the value of your bike and keep
+					up with regular maintenance to potentially increase its value.
 				</p>
 			</section>
 		</div>
