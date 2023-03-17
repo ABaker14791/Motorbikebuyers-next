ef45010b5bd7994e8771c3dce14a7fe6a63025a9
Adam Baker <adam_baker@live.co.uk>
2023-03-17 21:18:26 +0000

    added gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,33 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import Script from "next/script";
 import "../styles/globals.css";
 import { store } from "../store/store";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Provider store={store}>
-				<Component {...pageProps} />
-			</Provider>
-		</Layout>
+		<>
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TRACKING_TAG}`}
+			/>
+			<Script
+				id="google-analytics"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+					window.dataLayer = window.dataLayer || [];
+					gtag('js', new Date());
+					gtag('config', '${process.env.GOOGLE_TRACKING_TAG}', {
+					page_path: window.location.pathname,`,
+				}}
+			/>
+			<Layout>
+				<Provider store={store}>
+					<Component {...pageProps} />
+				</Provider>
+			</Layout>
+		</>
 	);
 }
---
+++ pages/article.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Head from "next/head";
+
+const article = () => {
+	return (
+		<div>
+			<Head>
+				<title>Motorbike Buyers - Motorcycle Maintenance Guide</title>
+				<meta name="description" content="Motorcycle Maintenance Guide" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<section>
+				<p>
+					Maintaining your motorcycle is essential for safe and enjoyable
+					riding, but did you know that it can also affect the value of your
+					motorbike? Whether you're buying a motorbike or wondering how much
+					your current bike is worth, regular maintenance can make a significant
+					difference. If you're buying a motorbike, it's essential to consider
+					the bike's condition and maintenance history. A well-maintained
+					motorcycle can indicate that the previous owner cared for the bike and
+					may have fewer issues than a poorly maintained one. Additionally, if
+					you're wondering how much a motorbike is worth, the bike's condition
+					and maintenance can affect its value. One way to determine how much
+					your motorbike is worth is to use a value my motorbike tool. These
+					tools take into account the bike's make and model, age, condition, and
+					maintenance history to provide an estimate of its value. By keeping up
+					with regular maintenance, you can potentially increase the value of
+					your motorbike and get a higher estimate when using a value my
+					motorbike tool. Regularly checking the oil level, inspecting the
+					tires, maintaining the chain, checking the brakes, inspecting the
+					battery, and keeping your bike clean are all important aspects of
+					motorcycle maintenance that can affect its value. For example, a bike
+					with worn tires, a loose chain, or leaking brake fluid may have a
+					lower value than one with properly maintained components. In
+					conclusion, maintaining your motorcycle not only ensures safe and
+					enjoyable riding but can also affect its value. Whether you're buying
+					a motorbike or wondering how much your current bike is worth, regular
+					maintenance can make a significant difference. Consider using a value
+					my motorbike tool to estimate the value of your bike and keep up with
+					regular maintenance to potentially increase its value.
+				</p>
+			</section>
+		</div>
+	);
+};
+
+export default article;
