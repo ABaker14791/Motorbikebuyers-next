af7583707b496f183bc055a1e81acb28b9031acc
Adam Baker <adam_baker@live.co.uk>
2023-03-29 23:25:47 +0100

    Updated create contact api route to check if email already exists before attempting to add new

--- pages/api/mail/emailAddContact.js
+++ pages/api/mail/emailAddContact.js
@@ -1,43 +1,74 @@
-const SibApiV3Sdk = require("sib-api-v3-sdk");
-let defaultClient = SibApiV3Sdk.ApiClient.instance;
-
-let apiKey = defaultClient.authentications["api-key"];
-apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
-
-let apiInstance = new SibApiV3Sdk.ContactsApi();
-
 export default async function emailAddContact(req, res) {
 	let contact = req.body;
-	let createContact = new SibApiV3Sdk.CreateContact();
+	const url = "https://api.sendinblue.com/v3/contacts";
+	const email = contact.email;
+	const listIds = [6];
 
-	createContact = {
-		email: contact.email,
-		listIds: [6],
-		attributes: {
-			FIRSTNAME: contact.firstName,
-			LASTNAME: contact.lastName,
-		},
+	const headers = {
+		"Content-Type": "application/json",
+		"api-key": process.env.SENDINBLUE_API_KEY,
 	};
 
-	if (req.method === "POST") {
-		try {
-			apiInstance.createContact(createContact).then(
-				function (data) {
-					return res.status(200).json({
-						success: true,
-						message: "Contact added successfully",
+	fetch(`${url}/${email}`, {
+		method: "GET",
+		headers,
+	})
+		.then((response) => {
+			if (response.ok) {
+				// Handle existing contact
+				console.log("Email already exists.");
+				res.status(200).json({ message: "Email already exists." });
+			} else if (response.status === 404) {
+				// Handle new contact
+				const payload = {
+					email,
+					listIds,
+					attributes: {
+						FIRSTNAME: contact.firstName,
+						LASTNAME: contact.lastName,
+					},
+				};
+				fetch(url, {
+					method: "POST",
+					headers,
+					body: JSON.stringify(payload),
+				})
+					.then((response) => {
+						if (response.ok) {
+							console.log("Contact created successfully.");
+							return res
+								.status(200)
+								.json({ message: "Contact created successfully." });
+						} else {
+							response.json().then((data) => {
+								const errorMessage = data.message;
+								console.log(`Error creating contact: ${errorMessage}`);
+								return res
+									.status(500)
+									.json({ message: `Error creating contact: ${errorMessage}` });
+							});
+						}
+					})
+					.catch((error) => {
+						console.log(`Error creating contact: ${error.message}`);
+						return res
+							.status(500)
+							.json({ message: `Error creating contact: ${errorMessage}` });
 					});
-				},
-				function (error) {
-					console.error(error);
-				}
-			);
-		} catch (err) {
+			} else {
+				response.json().then((data) => {
+					const errorMessage = data.message;
+					console.log(`Error retrieving contact: ${errorMessage}`);
+					return res
+						.status(500)
+						.json({ message: `Error creating contact: ${errorMessage}` });
+				});
+			}
+		})
+		.catch((error) => {
+			console.log(`Error retrieving contact: ${error.message}`);
 			return res
 				.status(500)
-				.json({ error2: "Failed to add contact.", errorMessage: err });
-		}
-	} else {
-		return res.status(500).json({ error3: "Failed to add contact." });
-	}
+				.json({ message: `Error creating contact: ${errorMessage}` });
+		});
 }
